21ed827a7b2aaf49efc57f13e7045033c2d9f3d8
mahesh pushpad <103810433+maheshpushpad@users.noreply.github.com>
2024-05-13 23:51:46 +0530

    Add files via upload

---
+++ AccountingSoftware/src/main/java/com/mycompany/accountingsoftware/AccountingSoftware.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.accountingsoftware;
+
+/**
+ *
+ * @author malak
+ */
+public class AccountingSoftware {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
